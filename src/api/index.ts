5c4ddba104df8cd79efd7cbcba7ab13d776affd4
Caio Escorel <69322958+Caioesc@users.noreply.github.com>
2025-03-28 17:14:05 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,5 +4,6 @@ import { Movie } from "../Types";
 const BASE_URL = "http://localhost:3001"
 
 export const getMovies = async (): Promise<Movie[]> =>{
-    const response = await axios.get<Movie[]>(`${BASE_URL}/movies`)
+    const response = await axios.get<Movie[]>(`${BASE_URL}/movies`);
+    return response.data;
 }
